1b2c84cb284551f055ab7a1f60f19a47c24eb22c
Keith Low <keithlowc@gmail.com>
2019-08-02 14:41:42 -0400

    23

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -150,3 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = (root - 1)('static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = (root - 1)('media')
