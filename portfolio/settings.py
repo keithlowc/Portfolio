d6120dce1e95a9ab3a1c3e83010cd783ca61ebf4
Keith Low <keithlowc@gmail.com>
2019-08-04 16:08:13 -0400

    123

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -156,10 +156,23 @@ USE_TZ = True
 #     '/var/www/static/',
 # ]
 
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, '../portfolio/static/')
+# STATICFILES_DIRS = [
+#     BASE_DIR + '/static/admin',
+# ]
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../portfolio/static/')
-STATICFILES_DIRS = [
-    BASE_DIR + '/static/admin',
-]
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 
