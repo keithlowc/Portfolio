34292ca9952f18cbfbb6429f15a9937e3cb94671
Keith Low <keithlowc@gmail.com>
2019-08-04 17:30:50 -0400

    sdasda

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,9 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR + '/../templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
