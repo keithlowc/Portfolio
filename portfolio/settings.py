3f74fe250429326ea6693f83bb33a50d8337676d
Keith Low <keithlowc@gmail.com>
2019-08-04 22:06:35 -0400

    asdasd

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -155,6 +155,9 @@ print('Hosts: ', ALLOWED_HOSTS)
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "staticfiles/admin"),
+]
 
 print('Hello world another tr a')
 print('debug: ', DEBUG)
