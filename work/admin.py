cac19fe79035a491feea36133e2e8f61bb5da08b
Keith Low <keithlowc@gmail.com>
2019-08-12 16:28:11 -0400

    added form to admin and form collection

--- work/admin.py
+++ work/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Projects
+from .models import Projects, Contacts
 
 admin.site.register(Projects)
+admin.site.register(Contacts)
--- work/views.py
+++ work/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from background_task import background
+from django.contrib import messages
+from django.shortcuts import redirect
 
 from .models import Projects
 from .forms import AddContactsForm
@@ -10,6 +12,17 @@ import requests
 def show_site(request):
     projects = Projects.objects.all()
 
+    form = AddContactsForm()
+
+    if request.method == 'POST':
+        form = AddContactsForm(request.POST)
+        if form.is_valid():
+            contact = form.save(commit=False)
+            contact.save()
+            messages.success(request, 'Succesfully added comment')
+        else:
+            messages.warning(request, 'Something went wrong! Please check your comment')
+    
     form = AddContactsForm()
 
     context = {
