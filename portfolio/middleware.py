483a36eace8c4a5d8fe5b8d8e22ebc0368741476
Keith Low <keithlowc@gmail.com>
2019-08-13 16:05:02 -0400

    added async functionality for waking apps

--- portfolio/middleware.py
+++ portfolio/middleware.py
@@ -1,25 +1,40 @@
 from work.background_jobs import background_functions
 from work.models import Projects
 
+import asyncio
+import requests
+
 
 '''
 This middleware function checks if an user has opened the website and sends a signal
-to the django-background-tasks to wake up the apps on the portfolio by sending a GET
-request to them. Usually takes 30 seconds to wake an app. This helps to preload them.
+to AsyncIO to wake up the apps on the portfolio by sending a GET
+request to them in a Asynchronous manner. Usually takes 30 seconds to wake an app. This helps to preload them.
 '''
 class WakeAppsOnUserVisit:
     def __init__(self, get_response):
         self.get_response = get_response
     
     def __call__(self, request):
+        
+        urls_list = []
         is_home = request.path_info.startswith('')
 
         if is_home:
             projects = Projects.objects.all()
 
             for project in projects:
-                background_functions.wake_apps(project.project_url)
-                print('Sending request to background job')
+                urls_list.append(project.project_url)
+
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+        loop.run_in_executor(None, wake_apps, urls_list)
             
         return self.get_response(request)
+
+
+def wake_apps(urls_list):
+    for url in urls_list:
+        requests.get(url)
+        print('Sending requests - for: ', url)
 
