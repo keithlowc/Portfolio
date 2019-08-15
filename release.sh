274be48c589d297be9de4a82e52c744779190784
Keith Low <keithlowc@gmail.com>
2019-08-15 15:59:55 -0400

    fixed release.sh

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py collectstatic --noinput
-python3 manage.py process_tasks
+# python3 manage.py collectstatic --noinput
 echo 'It worked!!'
